614d5e45a092a84e395ec690a880fad2fda11264
JEAN-PIERRE luxon <jeanpier@informatique.univ-paris-diderot.fr>
2014-11-07 20:09:16 +0100

    ajout du fichier 'decompresseur.ml'. Il contiendra les fonctions de lecture et de decompresseion. Pour le moment, le fichier contient les commentaires de spécification.

---
+++ decompresseur.ml
@@ -0,0 +1,12 @@
+(**********************************************
+        Projet OCAML S5
+
+  Auteurs : Luxon JEAN-PIERRE & Kahina RAHANI
+
+ **********************************************)
+
+(****************************************
+  Fichier : decompresseur.ml
+
+  Fichier en charge de la decompression
+ ****************************************)
